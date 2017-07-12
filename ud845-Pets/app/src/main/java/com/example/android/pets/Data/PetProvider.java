f0407057072244fa13845e8c1f8ba3e4b272d889
Masquerade <masquerade0097@gmail.com>
2017-07-12 18:37:40 +0530

    Added Uri Matcher

--- ud845-Pets/app/src/main/java/com/example/android/pets/Data/PetProvider.java
+++ ud845-Pets/app/src/main/java/com/example/android/pets/Data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.android.pets.Data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -13,6 +14,25 @@ public class PetProvider extends ContentProvider {
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
     private PetDbHelper mDbHelper;
+
+    /** URI matcher code for the content URI for the pets table */
+    private static final int PETS = 100;
+
+    /** URI matcher code for the content URI for a single pet in the pets table */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+    }
+
 
     /**
      * Initialize the provider and the database helper object.
